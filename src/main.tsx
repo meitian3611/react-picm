246434ebb709fb84bf7d11a753892ef69a61d1a9
v_tianmei <v_tianmei@tencent.com>
2026-07-24 18:15:51 +0800

    push

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "@/router";
 
-
 import { ConfigProvider } from "antd";
 import zhCN from "antd/locale/zh_CN";
 import "dayjs/locale/zh-cn";
-import "./index.scss";
+import "./init.scss";
 
 createRoot(document.getElementById("root")!).render(
   <ConfigProvider locale={zhCN}>
